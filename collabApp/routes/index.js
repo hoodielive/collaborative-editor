9cfc40108b6acaa104e92a5fad56d09612e122f1
hoodielive <hoodielive@gmail.com>
2019-05-11 10:39:44 -0400

    made a route for about page and another insertion in prep for contact page

--- collabApp/routes/index.js
+++ collabApp/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gemini' });
+  res.render('index', { title: 'Collab - a platform for sharing code' });
 });
+
+router.get('/about', function(req, res, next) {
+  res.render('about', {title: 'Collab - a platform for sharing code'})
+}); 
 
 module.exports = router;
