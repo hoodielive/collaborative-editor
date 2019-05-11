23909dc00cf02783930cc9732736c0292b398a92
hoodielive <hoodielive@gmail.com>
2019-05-11 12:42:20 -0400

    fix contact router - requires a callback

--- collabApp/routes/index.js
+++ collabApp/routes/index.js
@@ -10,4 +10,12 @@ router.get('/about', function(req, res, next) {
   res.render('about', {title: 'Collab - a platform for sharing code'})
 }); 
 
+router.route('/contact')
+  .get('/contact', function(req, res, next) {
+    res.render('contact', {title: 'Collab - a platform for sharing code'})
+  })
+  .post('/thank', function(req, res, next) {
+    res.render('thank', {title: 'Collab - a platform for sharing code'})
+  })
+
 module.exports = router;
